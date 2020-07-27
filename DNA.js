023f0a2a580455aa79ed77e1f7baf2f801a093e6
amaansupariwala <45778662+amaansupariwala@users.noreply.github.com>
2020-07-26 22:07:46 -0400

    Create DNA.js

---
+++ DNA.js
@@ -0,0 +1,41 @@
+function DNA(genes){
+  
+  if (genes){
+    this.genes = genes;
+  } else {
+    this.genes = [];
+    for (var i = 0; i < lifespan; i++){
+      this.genes[i] = p5.Vector.random2D();
+      this.genes[i].setMag(maxForce)
+    }
+  }
+  
+  
+  
+  this.combineDNA = function(partner){
+    
+    var newGenes = [];
+    var midPoint = floor(random(this.genes.length));
+    for (var i = 0; i < this.genes.length; i++){
+      if (i > midPoint){
+        newGenes[i] = this.genes[i];
+      } else {
+        newGenes[i] = partner.genes[i];
+      }
+    }
+    return new DNA(newGenes);
+      
+  } 
+  
+  this.mutation = function(){
+    for (var i = 0; i < this.genes.length; i++){
+      if (random(1) < 0.01) {
+        this.genes[i] = p5.Vector.random2D()
+        this.genes[i].setMag(maxForce);
+        
+      }
+    }
+  }
+  
+  
+}
